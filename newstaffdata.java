7673d00b1aa3f2b9df158b0f3ce6f378829f1b2d
Manuri Rupasinghe <97382451+ramnrupasinghe@users.noreply.github.com>
2022-10-07 05:19:57 +0530

    new staff sign up servlet - ramnrupasinghe

---
+++ newstaffdata.java
@@ -0,0 +1,84 @@
+
+package addstaff;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class newstaffdata extends HttpServlet {
+
+ 
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+          
+        }
+    }
+
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        PrintWriter out = response.getWriter();
+        
+        String firstname = request.getParameter("fname");        
+       String surname = request.getParameter("sname");
+       String id = request.getParameter("id");
+       String email = request.getParameter("email");
+       String phonenumber = request.getParameter("phn");
+       String address = request.getParameter("addr");
+       String country = request.getParameter("con");
+       String city = request.getParameter("city");
+       String street = request.getParameter("street");
+       String postcode = request.getParameter("pcode");
+       String username = request.getParameter("uname");
+       String password = request.getParameter("psw");
+       String confirmpsw = request.getParameter("cpsw");
+       
+       
+       try{
+       
+           Connection con;
+           
+           Class.forName("com.mysql.jdbc.Driver");
+            out.print("Driver loaded");
+           con = DriverManager.getConnection("jdbc:mysql://localhost:3306/","root","root");
+       
+       System.out.println("Connected");
+       
+       String sql = "insert into adminaddstaff (firstname,surname,id,email,phonenumber,address,country,city,street,postcode,username,password,confirmpsw) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+       
+       PreparedStatement ps = con.prepareStatement(sql);
+       
+       ps.executeUpdate();
+       
+       RequestDispatcher rd = request.getRequestDispatcher("staffadd.jsp");
+       rd.forward(request, response);
+       
+       }catch(Exception e){
+         System.out.println("ERROR :: "+e.getMessage());
+       }
+        
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
